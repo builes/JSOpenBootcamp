50d6afda33f54bc2c596edc88130dee9ad7370d3
Mauricio Builes <rmbuilesm@unal.edu.co>
2022-05-27 08:24:01 -0500

    Tarea2.js

--- firstNodeProject/index.js
+++ firstNodeProject/index.js
@@ -1,3 +1,29 @@
-console.log(
-  "Hola, este es mi primer ejercicio con Node en el mejor Bootcamp de programación del mundo"
-);
+// tipos primitivos
+
+// number
+let n = 5;
+
+//string
+let saludo = "hola mundo";
+
+//booleans
+let bool = true;
+
+//null y undefined
+let a = null;
+let b = undefined;
+
+// null undefined and 0 todas evaluan en false
+
+// null y undefined. Ambos sirven para algo muy parecido, que es indicar la ausencia de valor. Lo que ocurre es que undefined es un valor que denota que no hay valor porque no se ha definido todavía, mientras que null se usa para indicar que no hay valor porque así lo hemos querido indicar expresamente.
+
+console.log(typeof saludo);
+
+// Estructuras de datos js
+// listas
+// objetos
+
+// Fechas
+// Libreria de apoyo  moment.js
+const fecha = new Date();
+console.log(fecha);
---
+++ firstNodeProject/tarea1.js
@@ -0,0 +1,12 @@
+const arr = [
+  "Mauricio",
+  29,
+  true,
+  new Date(1993, 02, 18),
+  {
+    tiulo: "retrato de Dorian Gray",
+    autor: "Oscar Wilde",
+    fecha: new Date(1890, 06, 20),
+  },
+];
+console.log(arr);
